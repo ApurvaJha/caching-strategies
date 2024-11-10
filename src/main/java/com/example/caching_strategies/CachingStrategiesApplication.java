696f40b5269394d057c834edcc7fca51884e0805
Yash Ladia <ladiayash@gmail.com>
2024-11-10 06:10:27 +0530

    First commit

---
+++ src/main/java/com/example/caching_strategies/CachingStrategiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.caching_strategies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingStrategiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingStrategiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caching_strategies/CachingStrategiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caching_strategies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingStrategiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
